ff5efba6fc8c2fd054a32ad550f7cba7046dbc96
bbbnam <bbbnam@naver.com>
2020-08-25 01:52:59 +0900

    refactor : 큰수만들기 구현

--- src/main/java/programmers/bbbnam/큰수만들기/BigNumber.java
+++ src/main/java/programmers/bbbnam/큰수만들기/BigNumber.java
@@ -3,16 +3,29 @@ package programmers.bbbnam.큰수만들기;
 public class BigNumber {
 
     public String solution(String number, int k) {
-        String answer = "";
-        int right = number.length() - k;
+        //String answer = "";
+        int cnt = number.length() - k;
+        int right = number.length() - cnt;
         int left = 0;
         char [] charArray = number.toCharArray();
-        char [] answerArray = new char[number.length()];
+        char [] answerArray = new char[number.length() - k];
         int temp = 0;
-        int max = 0;
-
-        while () {
+        int index = 0;
+        while (cnt > 0) {
             //TODO 재작성
+            int max = 0;
+            for (int i = left; i <= right; i++) {
+               int num = Integer.parseInt(String.valueOf(charArray[i]));
+               if (num > max) {
+                   temp = i;
+                   max = num;
+               }
+            }
+            answerArray[index] = (char)(max + '0');
+            cnt--;
+            left = temp + 1;
+            right = number.length() - cnt;
+            index++;
         }
 
         return String.valueOf(answerArray);

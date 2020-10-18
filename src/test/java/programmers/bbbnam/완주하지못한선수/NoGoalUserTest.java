96fc818da92310ffb963b66572bb6a531d1f6dc6
bbbnam <bbbnam@naver.com>
2020-10-19 00:09:39 +0900

    test : level1 #3 testCode

--- src/test/java/programmers/bbbnam/완주하지못한선수/NoGoalUserTest.java
+++ src/test/java/programmers/bbbnam/완주하지못한선수/NoGoalUserTest.java
@@ -1,12 +1,32 @@
 package programmers.bbbnam.완주하지못한선수;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NoGoalUserTest {
 
-    @Test
-    void solution() {
+    @ParameterizedTest
+    @MethodSource("provideNames")
+    void solution(String [] participant, String [] completion, String result) {
+        NoGoalUser noGoal = new NoGoalUser();
+        assertThat(noGoal.solution(participant, completion)).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> provideNames() {
+        return Stream.of(
+            Arguments.of(new String [] {"leo", "kiki", "eden"},
+                        new String [] {"eden", "kiki"}, "leo"),
+            Arguments.of(new String [] {"marina", "josipa", "nikola", "vinko", "filipa"},
+                        new String [] {"josipa", "filipa", "marina", "nikola"}, "vinko"),
+            Arguments.of(new String [] {"mislav", "stanko", "mislav", "ana"},
+                        new String [] {"stanko", "ana", "mislav"}, "mislav")
+        );
     }
 }

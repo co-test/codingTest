a96c7727a3bf2f30c0aa3fd5a011aa1f2bae0f31
bbbnam <bbbnam@naver.com>
2020-10-18 23:53:13 +0900

    solve : level1 #1 solve

--- src/main/java/programmers/bbbnam/두개뽑아서더하기/PeekAndAdd.java
+++ src/main/java/programmers/bbbnam/두개뽑아서더하기/PeekAndAdd.java
@@ -1,6 +1,26 @@
 package programmers.bbbnam.두개뽑아서더하기;
 
+import java.util.*;
+
 public class PeekAndAdd {
     //프로그래머스-level1_두개뽑아서더하기
     //https://programmers.co.kr/learn/courses/30/lessons/68644
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        Set<Integer> set = new TreeSet<>();
+        Arrays.sort(numbers);
+        for(int i = 0; i < numbers.length; i++) {
+            for (int t = 0; t < numbers.length; t++) {
+                if (i==t) continue;
+                set.add(numbers[i] + numbers[t]);
+            }
+        }
+        int i = 0;
+        answer = new int[set.size()];
+        for (int s : set) {
+            answer[i] = s;
+            i++;
+        }
+        return answer;
+    }
 }
--- src/test/java/programmers/bbbnam/두개뽑아서더하기/PeekAndAddTest.java
+++ src/test/java/programmers/bbbnam/두개뽑아서더하기/PeekAndAddTest.java
@@ -1,7 +1,26 @@
 package programmers.bbbnam.두개뽑아서더하기;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class PeekAndAddTest {
 
+    @ParameterizedTest
+    @MethodSource("provideTestArguments")
+    public void solutionTest(int [] numbers, int [] result){
+        PeekAndAdd pa = new PeekAndAdd();
+        assertArrayEquals(pa.solution(numbers), result);
+    }
+
+    private static Stream<Arguments> provideTestArguments(){
+        return Stream.of(
+                Arguments.of(new int [] {2,1,3,4,1}, new int [] {2,3,4,5,6,7}),
+                Arguments.of(new int [] {5,0,2,7}, new int [] {2,5,7,9,12})
+        );
+    }
 }

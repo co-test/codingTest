44b02015cbf85a2cb7be6d856a41fce2b6fa04ff
bbbnam <bbbnam@naver.com>
2020-10-18 23:05:40 +0900

    init : level1 sol#1

---
+++ src/main/java/programmers/bbbnam/두개뽑아서더하기/PeekAndAdd.java
@@ -0,0 +1,6 @@
+package programmers.bbbnam.두개뽑아서더하기;
+
+public class PeekAndAdd {
+    //프로그래머스-level1_두개뽑아서더하기
+    //https://programmers.co.kr/learn/courses/30/lessons/68644
+}
---
+++ src/test/java/programmers/bbbnam/두개뽑아서더하기/PeekAndAddTest.java
@@ -0,0 +1,7 @@
+package programmers.bbbnam.두개뽑아서더하기;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PeekAndAddTest {
+
+}

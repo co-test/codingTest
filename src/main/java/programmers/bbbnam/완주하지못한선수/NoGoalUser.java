a680a0d138b650e64d65cff9959a1e521669fce2
bbbnam <bbbnam@naver.com>
2020-10-19 00:02:33 +0900

    init : level1 #3 init

---
+++ src/main/java/programmers/bbbnam/완주하지못한선수/NoGoalUser.java
@@ -0,0 +1,10 @@
+package programmers.bbbnam.완주하지못한선수;
+
+public class NoGoalUser {
+    //완주하지 못한 선수
+    // https://programmers.co.kr/learn/courses/30/lessons/42576
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+}
---
+++ src/test/java/programmers/bbbnam/완주하지못한선수/NoGoalUserTest.java
@@ -0,0 +1,12 @@
+package programmers.bbbnam.완주하지못한선수;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoGoalUserTest {
+
+    @Test
+    void solution() {
+    }
+}

e13d3b53e9bd58829d404833384166fa63444419
bbbnam <bbbnam@naver.com>
2020-10-18 23:56:07 +0900

    init : level1 #2 init

---
+++ src/main/java/programmers/bbbnam/크레인인형뽑기/Crane.java
@@ -0,0 +1,10 @@
+package programmers.bbbnam.크레인인형뽑기;
+
+public class Crane {
+    //크레인 인형뽑기 게임
+    // https://programmers.co.kr/learn/courses/30/lessons/64061
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/programmers/bbbnam/크레인인형뽑기/CraneTest.java
@@ -0,0 +1,13 @@
+package programmers.bbbnam.크레인인형뽑기;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CraneTest {
+
+    @Test
+    void solution() {
+
+    }
+}

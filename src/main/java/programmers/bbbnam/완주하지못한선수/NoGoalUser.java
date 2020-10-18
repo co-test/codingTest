3865bb5686563ce8ae8c6cc986641f5f7515c989
bbbnam <bbbnam@naver.com>
2020-10-19 00:14:28 +0900

    solve : level1 #3 solve

--- src/main/java/programmers/bbbnam/완주하지못한선수/NoGoalUser.java
+++ src/main/java/programmers/bbbnam/완주하지못한선수/NoGoalUser.java
@@ -1,10 +1,21 @@
 package programmers.bbbnam.완주하지못한선수;
 
+import java.util.Arrays;
+
 public class NoGoalUser {
     //완주하지 못한 선수
     // https://programmers.co.kr/learn/courses/30/lessons/42576
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        return answer;
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i = 0;
+        while (i < completion.length) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+            i++;
+        }
+        return participant[i];
     }
 }
